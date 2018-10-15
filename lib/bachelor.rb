e023927c83be1d81b35ec47d2c0ab5f09e1123b3
Roberto Samia <bojosteph@gmail.com>
2018-10-15 17:07:43 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,40 +1,86 @@
- require 'json'
- 
-data = JSON.parse(File.read('spec/fixtures/contestants.json')) 
-
-def get_first_name_of_season_winner(data, season)
+ def get_first_name_of_season_winner(data, season)
   # code here
-  winner = nil
-  data.each do |season_data, contestant_data|
-    if season_data == season_data
-    contestant_data.collect do |contestant|
-      if contestant["statuts"] =="Winner"
-      winner = contestant["name"].split[0]
+# binding.pry
+  
+    data[season].each do |people|
+
+      people.each do |k,v|
+        if v == "Winner"
+            full_name = people["name"]
+            return full_name.split(' ').first
+        end 
+      end
     end
-   end
- end
- winner
 end
+
 
 
 def get_contestant_name(data, occupation)
+
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
   # code here
-  contestant_name = []
-  data.each do ||season, contestant_data|
-    contestant_data.each do |contestant|
-      if contestant["name"] != "Sam Grover"
-        contestant["occupation"] == occupation
-        return_name.push (conte)
 end
+
+
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+counter = 0
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+counter
+
 end
+
+
+
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
+
 end
+
+
+
 
 def get_average_age_for_season(data, season)
   # code here
+age = 0
+count = 0
+  data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+        age += v.to_f
+      end
+    end
+  end
+
+  answer = (age/count).round
+return answer
 end

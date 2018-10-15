a2793c125a4abf69903f93e58c5faefba841ec9c
Roberto Samia <bojosteph@gmail.com>
2018-10-15 16:44:05 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,17 @@
-
-
+ require 'json'
+ 
+data = JSON.parse(File.read('spec/fixtures/contestants.json')) 
 
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = nil
+  data.each do |season_data, contestant_data|
+    if season_data == season_data
+    contestant_data.collect do |contestant|
+      if contestant["statuts"] =="Winner"
+      winner = contestant["name"].split[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
